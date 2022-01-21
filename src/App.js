cee16d61039dcf88983fb8393e76af4943e9548d
patricio0312rev <review0312oficial@gmail.com>
2022-01-20 22:56:40 -0500

    Creando componente LoginButton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/login-button.component';
 
 function App() {
-  const { loginWithRedirect } = useAuth0();
 
   return (
     <div className="App">
       <h1>Hello world </h1>
-      <button onClick={() => loginWithRedirect()}>Login</button>
+      <LoginButton />
     </div>
   );
 }
