60b7065f6876f7bd19500f9ca755d49ec034d658
patricio0312rev <review0312oficial@gmail.com>
2022-01-20 23:01:03 -0500

    Imprimiendo datos de auth0 en componente Profile

--- src/components/profile.component.jsx
+++ src/components/profile.component.jsx
@@ -1,10 +1,20 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const Profile = () => {
+    const { user } = useAuth0();
 
     return(
         <div>
-            Profile
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+
+                <pre>
+                    {JSON.stringify(user)}
+                </pre>
+            </div>
         </div>
     );
 }
