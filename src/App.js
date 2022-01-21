254836ba2ee1c007bc2145bdd89f6e3442309bbe
patricio0312rev <review0312oficial@gmail.com>
2022-01-20 23:11:47 -0500

    Condicionando renderizado con isAuthenticated e isLoading

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import LoginButton from './components/login-button.component';
 import LogoutButton from './components/logout-button.component';
 import Profile from './components/profile.component';
 
+import { useAuth0 } from '@auth0/auth0-react';
+
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  
+  if(isLoading) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <div className="App">
       <h1>Hello world </h1>
-      <LoginButton />
+      {
+        isAuthenticated ? <LogoutButton /> : <LoginButton />
+      }
       <Profile />
-      <LogoutButton />
     </div>
   );
 }
